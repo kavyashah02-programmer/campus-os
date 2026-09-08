1898574ece314df73f87ec06532d241bf70ba5af
Kavya Shah <kavya.tshah02@gmail.com>
2026-09-09 02:17:56 +0530

    Fixing Crash 3

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,7 @@ import FinanceTracker from './FinanceTracker';
 import RewardSystem from './RewardSystem';
 import Library from './Library';
 import AnimalDonation from './AnimalDonation';
-import MotivationalQuotes from './MotivationalQuotes';
-
+import MotivationalQuote from './MotivationalQuote';
 
 const MAX_DEVICES = 2;
 
